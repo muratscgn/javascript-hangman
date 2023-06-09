cbd19ade3efefbf51742a3dc589cf61ce3387107
Murat Seçgin <muratsecgin@Murat-MacBook-Air.local>
2023-06-09 11:50:01 +0300

    project completed

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const word_element = document.getElementById("word");
 const popup = document.getElementById("popup-container");
 const message_element = document.getElementById("success-message");
+const wrongLetters_element = document.getElementById("wrong-latters");
+const items = document.querySelectorAll(".item");
+const message_againLetter = document.getElementById("message");
+const playAgainButton = document.getElementById("play-again");
 
-const correctLetters = ["s", "u", "n", "d", "a", "y"];
+const correctLetters = [];
 const wrongLetters = [];
+let selectedWord = getRandomWord();
+
 
 function getRandomWord() {
     const words = [
@@ -20,8 +26,6 @@ function getRandomWord() {
 }
 
 function displayWord() {
-    const selectedWord = getRandomWord();
-
     word_element.innerHTML = `
         ${selectedWord.split("").map(letter => `
             <div class="letter">
@@ -36,5 +40,68 @@ function displayWord() {
         message_element.innerText = "Congratulations You Won";
     }
 }
+
+function updateWrongLetters() {
+    wrongLetters_element.innerHTML = `
+    ${wrongLetters.length > 0 ? "<h3>Wrong Letters</h3>" : ""}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    items.forEach((item, index) => {
+        const errorCount = wrongLetters.length;
+
+        if (index < errorCount) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    })
+
+    if (wrongLetters.length === items.length) {
+        popup.style.display = "flex";
+        message_element.innerText = "Sorry You Lost";
+    }
+}
+
+function displayMessage() {
+    message_againLetter.classList.add("show");
+
+    setTimeout(function () {
+        message_againLetter.classList.remove("show");
+    }, 2000)
+}
+
+playAgainButton.addEventListener("click", function () {
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = getRandomWord();
+    displayWord();
+    updateWrongLetters();
+
+    popup.style.display = "none";
+});
+
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+                displayMessage();
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+                updateWrongLetters();
+            } else {
+                displayMessage();
+            }
+        }
+    }
+});
 
 displayWord();
